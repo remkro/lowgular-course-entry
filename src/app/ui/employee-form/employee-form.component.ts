910be41ba6a482218b58c2a0b6c9ff4a26dfe07c
Remigiusz Królak <krolak.remek@gmail.com>
2022-11-16 12:59:41 +0100

    added display alert with form data

--- src/app/ui/employee-form/employee-form.component.ts
+++ src/app/ui/employee-form/employee-form.component.ts
@@ -13,4 +13,8 @@ export class EmployeeFormComponent {
     age: new FormControl(null, [Validators.min(18), Validators.required]),
     salary: new FormControl(null, Validators.required)
   });
+
+  onButtonClicked(form: {name:string, age:string, salary:string}) {
+    alert('User was successfully added to the database. Name: ' + form.name + ", Age: " + form.age + ", Salary: " + form.salary);
+  }
 }
