9fa82f1545217f5dd2aa8a4f6a68d58c8704a4fd
Remigiusz Królak <krolak.remek@gmail.com>
2022-11-23 19:33:26 +0100

    added display form and list under routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './ui/home/home.component';
+import { EmployeeFormComponent } from './ui/employee-form/employee-form.component';
+import { EmployeeListComponent } from './ui/employee-list/employee-list.component';
+import { EmployeeListComponentModule } from './ui/employee-list/employee-list.component-module';
+import { EmployeeServiceModule } from './services/employee.service-module';
+import { EmployeeFormComponentModule } from './ui/employee-form/employee-form.component-module';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: HomeComponent }, { path: 'create-employee', component: EmployeeFormComponent }, { path: 'employee-list', component: EmployeeListComponent }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), EmployeeListComponentModule, EmployeeServiceModule, EmployeeFormComponentModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
